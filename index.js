b85e72032b93df1dbe1aa3c78ce6c90a2bcfac64
friendsim2 <75440424+friendsim2@users.noreply.github.com>
2020-12-03 22:01:34 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,6 @@
 var layer1 = document.getElementById("layer1");
 
 //game
-function everything(){
-  
+
   layer1.style.backgroundColor = "#F00";
-  
-window.reqestAnimatioFrame(everything);
-}
-window.reqestAnimatioFrame(everything);
+
