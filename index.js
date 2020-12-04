eb77578ddfe17a83d6bbe4b7c5eff3505cef51aa
friendsim2 <75440424+friendsim2@users.noreply.github.com>
2020-12-03 21:41:02 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 var canvas = document.getElementById("canvas").style;
 
 //game
-window.reqestAnimatioFrame(everything);
 function everything(){
   
   document.getElementById("canvas").style.backgroundColor = "#888";
   
 window.reqestAnimatioFrame(everything);
 }
+window.reqestAnimatioFrame(everything);
