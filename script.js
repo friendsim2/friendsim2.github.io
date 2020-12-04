a8c300f97feeacdbee88f32d32da1f8bbc53b5ac
friendsim2 <75440424+friendsim2@users.noreply.github.com>
2020-12-04 13:30:57 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 var canvas = document.getElementById("theCanvas");
+var w = 1280;
+var h = 720;
 
 function game() {
-  if (window.innerWidth/16 > window.innerHeight/9){
-    canvas.style.height = window.innerHeight;
-    canvas.style.width = canvas.style.height*(16/9);
+  w = window.innerWidth;
+  h = window.innerHeight;
+  if (w/16 > h/9){
+    canvas.style.height = h;
+    canvas.style.width = h*(16/9);
   } else {
-    canvas.style.width = window.innerWidth;
-    canvas.style.height = canvas.style.width*(9/16);
+    canvas.style.width = w;
+    canvas.style.height = w*(9/16);
   }
   
 requestAnimationFrame(game);}
