f8fde01ef95fe1ed57eaead40bf0a9c97814987a
friendsim2 <75440424+friendsim2@users.noreply.github.com>
2020-12-04 15:12:49 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
+// These make sure the canvas looks right
 var canvas = document.getElementById("theCanvas");
 var w = 1280;
 var h = 720;
 
+// Game time
 function game() {
+  // The following makes the canvas fit the screen while keeping aspect ratio.
   w = window.innerWidth;
   h = window.innerHeight;
   if (w/16 > h/9){
     canvas.style.height = h;
     canvas.style.width = h*(16/9);
+    canvas.style.left = (w-h*(16/9))/2;
   } else {
     canvas.style.width = w;
     canvas.style.height = w*(9/16);
+    canvas.style.top = (h-w*(9/16))/2;
   }
   
-requestAnimationFrame(game);}
+  
+  
+  
+  // These requestAnimationFrames start the game and keep it going.
+requestAnimationFrame(game);
+}
 requestAnimationFrame(game);
