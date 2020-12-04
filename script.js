57c9fb6976a2f582a800240653bf227b676e08a8
friendsim2 <75440424+friendsim2@users.noreply.github.com>
2020-12-04 13:19:33 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 var canvas = document.getElementById("theCanvas");
 
 function game() {
-  
-  canvas.style.width = "200";
+  if (window.innerWidth/16 > window.innerHeight/9){
+    canvas.style.height = window.innerHeight;
+    canvas.style.width = canvas.style.height*(16/9);
+  } else {
+    canvas.style.width = window.innerWidth;
+    canvas.style.height = canvas.style.width*(9/16);
+  }
   
 requestAnimationFrame(game);}
 requestAnimationFrame(game);
