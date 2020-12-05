b128d1fd486657b01a5d239c0d96dab52d6a593e
friendsim2 <75440424+friendsim2@users.noreply.github.com>
2020-12-04 23:20:45 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,11 @@
 var gameWindow = document.getElementById("gameWindow");
 var w = 1280;
 var h = 720;
+var dialogue = document.getElementById("dialogue");
 
 // Game time
 function game() {
-  // The following makes the game window fit the browser window while keeping aspect ratio.
+  // The following sets of lines keep everything the correct size or aspect ratio.
   w = window.innerWidth;
   h = window.innerHeight;
   if (w/16 > h/9){
@@ -15,7 +16,8 @@ function game() {
     gameWindow.style.width = w;
     gameWindow.style.height = w*(9/16);
   }
-  
+  // More aspect ratio stuff
+  dialogue.style.fontSize = gameWindow.style.height*(38/1080);
   
   
   
