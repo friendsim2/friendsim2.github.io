092094c08a8cb76b07d8395d1b30e57dbff4985a
friendsim2 <75440424+friendsim2@users.noreply.github.com>
2020-12-05 00:22:26 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 var gameWindow = document.getElementById("gameWindow");
 var w = 1280;
 var h = 720;
+var gameW = 0;
+var gameH = 0;
 
 // Game time
 function game() {
@@ -16,8 +18,10 @@ function game() {
     gameWindow.style.height = w*(9/16);
   }
   // More aspect ratio stuff
-  document.getElementById("dialogueBox").style.width = gameWindow.style.width*(1390/1920);
-  document.getElementById("dialogueBox").style.height = gameWindow.style.height*(262/1080);
+  gameW = gameWindow.style.width;
+  hameh = gameWindow.style.height;
+  document.getElementById("dialogueBox").style.width = gameW*(1390/1920);
+  document.getElementById("dialogueBox").style.height = gameH*(262/1080);
   document.getElementById("dialogue").style.fontSize = h*(38/1080);
   
   
