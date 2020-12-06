ce9ad9508ebe5d70398deb0fa5949d685cee9774
friendsim2 <75440424+friendsim2@users.noreply.github.com>
2020-12-06 14:21:02 -0600

    Update script.js

--- script.js
+++ script.js
@@ -30,6 +30,11 @@ function game() {
   changeChildren("#textbox", "width", "200px");
   changeChildren("#textbox", "borderLeft", "50px solid transparent");
   changeChildren("#textbox", "borderRight", "50px solid transparent");
+  document.getElementById("contentTop").style.borderBottom = "25px solid #555";
+  document.getElementById("contentBottom").style.borderTop = "25px solid #555";
+  changeChildren("#contentTop", "width", "100px");
+  changeChildren("#contentTop", "borderLeft", "25px solid transparent");
+  changeChildren("#contentTop", "borderRight", "25px solid transparent");
   
   
   // These requestAnimationFrames start the game and keep it going.
