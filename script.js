bc10af82a9ceb388c82ca4eabb79ff8b449aec4e
friendsim2 <75440424+friendsim2@users.noreply.github.com>
2020-12-06 17:35:46 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ var windowHeight = 720;
 var width;
 var height;
 
-function changeChildren(parent, cssStyle, newStyle) {
-  let x = document.querySelector(parent).children;
+function styleAll(elements, cssStyle, newStyle) {
+  let x = document.querySelectorAll(elements);
   for (let i = 0; i < x.length; i++) {
     x[i].style[cssStyle] = newStyle;
   }
@@ -27,14 +27,14 @@ function game() {
   document.querySelector("#gameWindow").style.height = height;
   document.querySelector("#textbox > #border > #top").style.borderBottom = "100px solid #777";
   document.querySelector("#textbox > #border > #bottom").style.borderTop = "100px solid #777";
-  changeChildren("#textbox > #border", "width", "200px");
-  changeChildren("#textbox > #border", "borderLeft", "50px solid transparent");
-  changeChildren("#textbox > #border", "borderRight", "50px solid transparent");
+  styleAll("#textbox > #border > *", "width", "200px");
+  styleAll("#textbox > #border > *", "borderLeft", "50px solid transparent");
+  styleAll("#textbox > #border > *", "borderRight", "50px solid transparent");
   document.querySelector("#textbox > #content > #top").style.borderBottom = "25px solid #BBB";
   document.querySelector("#textbox > #content > #bottom").style.borderTop = "25px solid #BBB";
-  changeChildren("#textbox > #content", "width", "100px");
-  changeChildren("#textbox > #content", "borderLeft", "25px solid transparent");
-  changeChildren("#textbox > #content", "borderRight", "25px solid transparent");
+  styleAll("#textbox > #content > *", "width", "100px");
+  styleAll("#textbox > #content > *", "borderLeft", "25px solid transparent");
+  styleAll("#textbox > #content > *", "borderRight", "25px solid transparent");
   
   
   // These requestAnimationFrames start the game and keep it going.
