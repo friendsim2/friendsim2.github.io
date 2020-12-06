3396de995c073cf2eb93273a82f24a26b72d20df
friendsim2 <75440424+friendsim2@users.noreply.github.com>
2020-12-06 00:06:46 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 // Variables
 var windowWidth = 1280;
 var windowHeight = 720;
+var textboxPart = document.getElementById("tetbox").getElementById("borderTop");
 
 // Functions used for the game
 // The following keeps everything the correct size.
 function setDimensions(width, height){
   document.getElementById("gameWindow").style.width = width;
   document.getElementById("gameWindow").style.height = height;
+  textboxPart = document.getElementById("tetbox").childNodes;
+  textboxPart.style.width = 0px;
+  textboxPart.style.borderLeft = 25px solid transparent;
+  textboxPart.style.borderRight = 25px solid transparent;
+  document.getElementById("borderTop").borderBottom: 50px solid #555;
+  document.getElementById("borderBottom").borderTop: 50px solid #555;
+  
   document.getElementById("dialogueText").style.fontSize = height*(34/1080);
 }
 
